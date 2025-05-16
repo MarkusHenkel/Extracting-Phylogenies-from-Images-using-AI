280850360803bc16d6ec3ede1e6897f96c963a4b
Markus <markus.henkel04@gmail.com>
2025-05-16 17:03:36 +0200

    replaced taxids in the images with the corresponding taxa and implemented automatic saving to file

--- data-collection/phylogenetic_trees_and_newicks.py
+++ data-collection/phylogenetic_trees_and_newicks.py
@@ -2,26 +2,29 @@ from Bio import Phylo
 from ete3 import NCBITaxa
 from io import StringIO 
 from numpy import random
+import datetime
 import argparse
 import matplotlib.pyplot as plt
+import re
+
+# TODO: replace comments with pydocs
 
 # ncbi taxonomy
 ncbi = NCBITaxa()
 
 # Check if a given taxid is valid 
 def is_valid(taxid):
-    if ncbi.get_taxid_translator([taxid]):
-        return True
-    else:
-        return False
+    return True if ncbi.get_taxid_translator([taxid]) else False 
     
 # Counts amount of invalid taxids in the first 10000
+# Function for testing
 def amount_invalid():
     counter = 0
     for taxid in range(10000):
         if not is_valid(taxid):
             counter += 1
     return counter
+
 # Given a preferred number of taxa (amount as int) and a boolean generates either a list with fixed or randomized amount [2, amount] of random valid taxa
 #
 # default amount of taxa is 10
@@ -45,7 +48,7 @@ def generate_random_taxids(amount = 10, randomize = False):
 
 # Given a list of random valid taxids returns a list of the corresponding taxa names 
 # 
-# output of get_taxid_translator is a dictionary where the keys are the taxon IDs and the values the 
+# output of get_taxid_translator is a dictionary where the keys are the taxon IDs and the values the taxa
 def taxids_to_taxa(valid_rand_taxids):  
     valid_rand_taxa_dict = ncbi.get_taxid_translator(valid_rand_taxids)
     valid_rand_taxa = []
@@ -64,18 +67,37 @@ def tree_to_image_file(newick):
     # make the the newick tree (string) into a file so that it can be drawn by bio.phylo (not elegant?)
     newick_file = StringIO(newick)
     newick_tree = Phylo.read(newick_file, "newick")
-    # create the figure
-    #fig = plt.figure()
-    #axes = fig.add_subplot(1,1,1)
-    # draw the tree on the axes
+    # create a matplotlib figure and 
     newick_tree.rooted = True
-    # Phylo.draw(newick_tree, axes = axes)
-    Phylo.draw(newick_tree)
-    #plt.savefig("phylo_tree.jpg")
-    #plt.close()
+    fig = plt.figure(figsize=(10, 20), dpi=150)
+    axes = fig.add_subplot(1, 1, 1)
+    Phylo.draw(newick_tree, axes=axes, do_show=False)
+    plt.savefig(f"tree_{str(datetime.datetime.now().strftime(r"%Y-%m-%d-%H-%S-%f"))}.jpg")
+    plt.close()
 
-# TODO: add function that edits newick file so that the generated images contain the taxa names not their IDs
+# Given a newick tree return a list of all taxids inside the string using regex
+# CAUTION: This regex only works with newicks with distances of 1 so before distances are added
+def taxids_from_newick(newick_string):
+    return re.findall(r"[^)(,:][\d]+[^:]", newick_string)
+    
+def translate_newick_tree(newick_string):
+    """
+    Given a newick tree with taxids returns the newick tree with the corresponding taxa
+
+    Args:
+        newick_string (str): The tree in newick format
+
+    Returns:
+        newick_string (str): The same tree but taxon IDs are replaced with the corresponding taxa
+    """
+    taxids = taxids_from_newick(newick_string)
+    taxa = taxids_to_taxa(taxids)
+    for i in range(len(taxids)):
+        newick_string = newick_string.replace(taxids[i], taxa[i])
+    return newick_string
+    
 # TODO: add function that edits newick file so that the distances of the newick tree are randomized before its drawn
+# TODO: add function that puts the image and a text file with the newick inside a folder and 
 
 def main():
     argument_parser = argparse.ArgumentParser(
@@ -86,12 +108,16 @@ def main():
     #
     
     # TODO: add parameter to make random distances optional (0 for standard distance of 1, 1 for randomized distances)
+    # TODO: change name of image file to something that increments
     
     # parameter for the preferred number of taxa generated, if not specified defaults to 10 taxa
     argument_parser.add_argument('-a', '--amount_taxa', required=False, type=int,
                                  help='Type=Int. Choose the preferred amount of generated taxa. If not specified defaults to 10 taxa.')
-    argument_parser.add_argument('-r', '--randomize_amount', required=False, action='store_true',
+    argument_parser.add_argument('-ra', '--randomize_amount', required=False, action='store_true',
                                  help='On/Off flag. Randomizes the amount of taxa generated. Range: 2 to <amount_taxa>. Per default the amount of taxa is not randomized.')
+    # argument_parser.add_argument('-rd', '--randomize_distances', required=False, type=int,
+    #                              help='On/Off flag. ')
+    
     # Specified parameters
     args = argument_parser.parse_args()
     
@@ -108,37 +134,36 @@ def main():
     # -a and -r specified
     if amount_taxa != None and randomize_amount == True:
         random_taxids = generate_random_taxids(amount=amount_taxa, randomize=True)
-        newick = generate_newick_tree(random_taxids)
-        print("foo")
+        newick_taxids = generate_newick_tree(random_taxids)
+        newick_taxa = translate_newick_tree(newick_taxids)
     # -a specified, -r=False
     elif amount_taxa != None and randomize_amount == False:
         random_taxids = generate_random_taxids(amount=amount_taxa)
-        newick = generate_newick_tree(random_taxids)
-        print("bar")
+        newick_taxids = generate_newick_tree(random_taxids)
+        newick_taxa = translate_newick_tree(newick_taxids)
     # -r specified, -a=None
     elif amount_taxa == None and randomize_amount == True:
         random_taxids = generate_random_taxids(randomize=True)
-        newick = generate_newick_tree(random_taxids)
-        print("far")
+        newick_taxids = generate_newick_tree(random_taxids)
+        newick_taxa = translate_newick_tree(newick_taxids)
     # neither specified, -a=None, -r=False
     else:
         random_taxids = generate_random_taxids()
-        newick = generate_newick_tree(random_taxids)
-        print("feng")
-    
+        newick_taxids = generate_newick_tree(random_taxids)
+        newick_taxa = translate_newick_tree(newick_taxids)
     # Generates the newick tree
-    print(newick)
-    tree_to_image_file(newick)
+    print(f"Randomize amount: {randomize_amount}. Specified amount of taxa: {amount_taxa}. Actual amount of taxa: {len(random_taxids)}.")
+    print("Newick tree:", newick_taxa)
+    tree_to_image_file(newick_taxa)
     
     
     
     #
     # Testing main functionality
     #
-    
-    print(f"Randomize amount: {randomize_amount}. Specified amount of taxa: {amount_taxa}. Actual amount of taxa: {len(random_taxids)}")
-    
-    
+    # print()
+    # print("With taxids:","(((((((8610:1,8834:1)1:1,9881:1)1:1,8343:1)1:1,7755:1)1:1,6957:1)1:1,(4466:1,4151:1)1:1)1:1,(1381:1,2366:1)1:1);")
+    # print("Translation:", translate_newick_tree("(((((((8610:1,8834:1)1:1,9881:1)1:1,8343:1)1:1,7755:1)1:1,6957:1)1:1,(4466:1,4151:1)1:1)1:1,(1381:1,2366:1)1:1);"))
     # taxid_list = generate_random_taxids(amount=10, randomize=False)
     # newick = generate_newick_tree(taxid_list)
     # print(len(taxid_list))
