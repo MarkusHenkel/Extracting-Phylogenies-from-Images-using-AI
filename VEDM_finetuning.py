07543be1d698a6aeb6e203d76d71f868ca78e435
Markus <markus.henkel04@gmail.com>
2025-07-14 10:19:55 +0200

    added load_dataset and preprocess_dataset functions and tried to implement pipeline: load preprocessed dataset, load model, tokenizer and imageprocessor and load trainer class with basic training arguments but couldnt solve errors yet

--- VEDM_finetuning.py
+++ VEDM_finetuning.py
@@ -1,14 +1,16 @@
 # model and model configs
-from transformers import VisionEncoderDecoderModel, GPT2TokenizerFast, ViTImageProcessor
+from transformers import VisionEncoderDecoderModel, AutoTokenizer, ViTImageProcessor, TrainingArguments, Trainer
+from datasets import Dataset
 import albumentations as A # for image augmentation
 import matplotlib.pyplot as plt
 import cv2 # 
 import os # for getting dataset from filesystem
-import argparse
-import requests
+# import requests
 from PIL import Image # 
-import torch
+# import torch
 import datetime
+import re
+import numpy as np # jumping between np arrays (albumentations) and pytorch tensors (model)
 
 # time for logging
 time = datetime.datetime.now().strftime("%Y-%b-%d %H:%M:%S")
@@ -16,32 +18,26 @@ time = datetime.datetime.now().strftime("%Y-%b-%d %H:%M:%S")
 # image size in px
 image_size = 2048
 
-# load a fine-tuned image captioning model and corresponding tokenizer and image processor
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = GPT2TokenizerFast.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-image_processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+# maximum amount of tokens in one newick
+max_token_length = 512
 
-"""
-Basic transform for training the baseline. Uniform res of <image_size>x<image_size>. 
-Breaks aspect ratio!
-"""
-transform_1 = A.Resize(image_size,image_size)
-"""
-Basic transform for training the baseline. Uniform res of <image_size>x<image_size>.
-Keeps aspect ratio!
-"""
-transform_1_ar = A.Compose(
+########## IMAGE AUGMENTATION ##########
+# Basic transform for training the baseline. Uniform res of <image_size>x<image_size>. Breaks aspect ratio!
+augment_1 = A.Resize(image_size,image_size)
+
+# Basic transform for training the baseline. Uniform res of <image_size>x<image_size>. Keeps aspect ratio!
+augment_1_ar = A.Compose(
     [
         A.LongestMaxSize(max_size=image_size, interpolation=1), # resize while keeping aspect ratio
-        A.PadIfNeeded(min_height=image_size, min_width=image_size, border_mode=0) # padding for square res
+        A.PadIfNeeded(min_height=image_size, min_width=image_size, border_mode=0), # padding for square res
+        A.Normalize(),
+        A.ToTensorV2()
     ]
 )
 
-"""
-Resize images to uniform resolution and augment them to get more samples and make model more robust
-towards different conditions of the image. Only used after baseline is established i.e. second training round
-"""
-transform_2 = A.Compose(
+# Resize images to uniform resolution and augment them to get more samples and make model more robust 
+# towards different conditions of the image. Only used after baseline is established i.e. second training round
+augment_2 = A.Compose(
     [
         A.LongestMaxSize(max_size=image_size, interpolation=1), # resize while keeping aspect ratio
         A.PadIfNeeded(min_height=image_size, min_width=image_size, border_mode=0), # padding for square res
@@ -54,88 +50,229 @@ transform_2 = A.Compose(
             # more square holes in grid format, non-overlapping
             A.GridDropout(ratio=0.2, p=1.0),
             # fewer but bigger square holes, , non-overlapping
-            A.GridDropout(ratio=0.1, unit_size_range=(500, 1000),p=1.0)
+            A.GridDropout(ratio=0.1, unit_size_range=(500, 1000),p=1.0),
         ], p=0.1), # apply dropout only every 10th image
+        A.Normalize(), # TODO: default values or not?
+        A.ToTensorV2()
         
     ]
 )
 
+def transform_1_ar(dataset):
+    dataset["pixel_values"] = [augment_1_ar(image=image) for image in dataset["image"]]
+    return dataset
+
+########## LOADING DATASET ##########
 def load_dataset(dataset_path):
     """
-    Given a path to a directory, iterates through all subdirs and add the pairs as tuples to a list
+    Given a path to a directory, iterates through all subdirs and creates a dict with three lists 
+    IDs, paths to each image and the newicks. Ordering is strict, the first image in the image list corresponds 
+    to the first newick in the newick list etc
 
     Args:
-        dataset_path (str): path to the dataset with subdirs each containing a image/newick pair
+        dataset_path (str): path to the dataset with subdirs that each have an id e.g. data202541627 and 
+        contain an image_path/newick pair
 
     Returns:
-        List((str, str)): list of image/newick tuples
+        dict: dict that contains 3 lists: IDs, paths to each image, newicks
     """
-    dataset = []
+    dataset = {
+        "id": [],
+        "image": [],
+        "newick": []
+    }
+    if not os.path.isdir(dataset_path):
+        exit(f"[{time}] Error in load_dataset: Given path is not a directory.")
+    # iterate over each subdirectory in the dataset directory
     for subdir, dirs, files in os.walk(dataset_path):
         image = ""
         newick = ""
+        # skip root of os.walk, the dataset itself which has no id etc because it only contains subdirs with pairs
+        if subdir == dataset_path:
+            continue
+        # assign each entry in the dataset an ID; looks for a number at the end of the name of the current subdir
+        if re.search(r"\d+$", subdir):
+            id = re.search(r"\d+$", subdir).group()
+        else: 
+            # skip over subdirectories without an id (TODO: maybe subdirs shouldnt be forced to have an ID, only img, nwk etc)
+            print(f"[{time}] Warning: Skipping subdir {subdir} because it does not have an ID in its name.")
+            continue
         for file in files:
             # support other file formats as well in case ptan module uses other formats in the future
             if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg") or file.endswith(".pdf"):
-                image = file
+                image = os.path.join(subdir, file)
             elif file.endswith(".nwk") or file.endswith(".txt"):
                 with open(os.path.join(subdir, file), "r") as nwk_file:
                     newick = nwk_file.read()
-                # TODO: warn user if dataset contains false newicks, check with is_newick from ptan
-        if image and newick:
-            dataset.append((image, newick))
+                # TODO: warn user if dataset contains false newicks?
+        if id and image and newick:
+            dataset["id"].append(id)
+            dataset["image"].append(image)
+            dataset["newick"].append(newick)
+            id = ""
+            image = ""
+            newick = ""
         else:
-            print(f"[{time}] Warning in load_dataset(): Subdir {subdir} did not contain image/nwk or image/txt pair.") 
+            print(f"[{time}] Warning in load_dataset(): Subdir {subdir} is missing image path or Newick.")
+            id = ""
+            image = ""
+            newick = ""
     return dataset
 
-def get_augmented_img_from_path(transform, image_path):
+def preprocess_dataset(dataset, tokenizer, image_processor):
+    """
+    Given a dict with 3 lists: ID, image path and newick returns a huggingface dataset object 
+    (https://huggingface.co/docs/datasets/v1.2.1/loading_datasets.html) containing the pixel values of the images and
+    tokenized newicks
+    
+    Args:
+        dataset (dict): dict with 3 lists: IDs, image paths, newicks
+
+    Returns:
+        dataset object: huggingface dataset object containing pixel values and tokenized newicks
+    """
+    # rename keys image and newick in the dataset dict (model expects keys to be pixel_values and labels)
+    dataset["pixel_values"] = dataset.pop("image")
+    dataset["labels"] = dataset.pop("newick")
+    # replace image and newick with the entries the model expects => pixel_values and labels
+    for i in range(len(dataset["id"])):
+        pil_image = Image.open(dataset["pixel_values"][i])
+        # TODO: pyarrow.lib.ArrowTypeError: Expected bytes, got a 'Tensor' object 
+        dataset["pixel_values"].append(image_processor(pil_image, return_tensors="pt").pixel_values[0])
+        dataset["labels"].append(tokenizer(
+            dataset["labels"][i], 
+            padding="max_length", # pad up to max length if necessary
+            max_length=max_token_length, # set maximum of tokens in each newick string
+            return_tensors="pt" # pytorch tensors
+        ).input_ids[0])
+    return Dataset.from_dict(dataset)
+
+########## COMPUTE METRICS ##########
+def compute_metrics(pred):
+    # TODO: add metrics used in module comparison_of_newicks.py
+    preds, label_ids = pred
+    print("###############")
+    print("preds:" + preds)
+    print("label_ids:" + label_ids)
+    print("###############")
+      
+########## FOR TESTING ##########
+def get_augmented_img_from_path(image_path, augmentation=augment_1_ar):
+    """
+    Given the path to an image, opens it using OpenCV and applies a given albumentations transform.
+
+    Args:
+        image_path (str): path to image
+        transform (albumentations transform, optional): Transform applied to given image. Defaults to augment_1_ar.
+
+    Returns:
+        pytorch tensor: numpy tensor containing the transformed image 
+    """
     try:
+        # loads image as numpy array in BGR format instead of RGB
         image = cv2.imread(image_path)
     except:
-        exit(f"[{time}] Error in get_augmented_img_from_path: Path could not be resolved.")
-    augmented = transform(image=image)['image']
-    augmented_rgb = cv2.cvtColor(augmented, cv2.COLOR_BGR2RGB)
-    return augmented_rgb
+        exit(f"[{time}] Error in get_augmented_img_from_path: Path {image_path} could not be resolved.")
+    # turn in into RGB format
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # apply transform
+    augmented_tensor = augmentation(image=image_rgb)['image']
+    return augmented_tensor
 
-def test_transforms(transform, image_path):
-    augmented_rgb = get_augmented_img_from_path(transform, image_path)
+def test_augmentation(augment, image_path):
+    augmented_tensor = get_augmented_img_from_path(augmentation=augment, image_path=image_path)
     plt.figure(figsize=[image_size, image_size], dpi=100)
-    plt.imshow(augmented_rgb)
+    # turn the tensor into numpy array 
+    plt.imshow(np.transpose(augmented_tensor.cpu().numpy(),(1,2,0)))
     plt.axis('off')
     plt.show()
     
-def perform_inference(image_path):
-    image = cv2.imread(image_path)
-    image_preprocessed = transform_1_ar(image=image)["image"]
-    pixel_values = image_processor(image_preprocessed, return_tensors="pt").pixel_values
+def get_number_tokens(newick, tokenizer):
+    return len(tokenizer(newick, return_tensors="pt").input_ids[0])
 
-    # autoregressively generate caption (uses greedy decoding by default)
+########## PERFORMING INFERENCE ##########
+def perform_inference_img_processor(image_path, model, image_processor, tokenizer):
+    """
+    Perform inference on an image using an image_processor e.g. the pretrained image processor used in finetuning
+
+    Args:
+        image_path (str): path to image
+        image_processor (image processor object, optional): image processor e.g. HF ViTImageProcessor. Defaults to pretrained_image_processor.
+    """
+    image = cv2.imread(image_path)
+    augmented_tensor = augment_1_ar(image=image)["image"]
+    # TODO: image processor likely expects a numpy array, not a pt tensor (bc it worked before, but ToTensorV2 was added)
+    # turn the tensor into numpy array 
+    augmented_np_arr = np.transpose(augmented_tensor.cpu().numpy(),(1,2,0))
+    pixel_values = image_processor(augmented_np_arr, return_tensors="pt").pixel_values
     generated_ids = model.generate(pixel_values)
     generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
     print(generated_text)
-
+    
 def main():
-    # argument parser for getting dataset
-    parser = argparse.ArgumentParser(description="Module for finetuning a Vision Encoder Decoder Model. Dataset must be\
-        created using module phylogenetic_tree_and_newicks.py")
-    # arguments
-    parser.add_argument("-d", "--dataset", required=False, type=str, help="Path to the dataset made with the\
-        phylogenetic_trees_and_newicks.py module.")
-    # get user arguments
-    args = parser.parse_args()
-    path_dataset = args.dataset 
-    if path_dataset:
-        dataset = load_dataset(path_dataset)
-    else:
-        print(f"[{time}] Error: Dataset could not be loaded.")
-    ########## Testing ##########
-    # ete3_20_taxa = r"C:\Users\marku\Desktop\StudiumVault\Semester6\Bachelorarbeit\Code\Extracting-Phylogenies-from-Images-using-AI\data_collection\dataset\ete3_20_taxa\data_2025-06-25-00-25-56-658225\ete3_tree_2025-06-25-00-25-56-658225.jpg"
-    # phylo_20_taxa = r"C:\Users\marku\Desktop\StudiumVault\Semester6\Bachelorarbeit\Code\Extracting-Phylogenies-from-Images-using-AI\data_collection\dataset\phylo_20_taxa\data_2025-06-25-00-25-33-042017\phylo_tree_2025-06-25-00-25-33-042017.jpg"
-    # phylo_rand_dist = r"C:\Users\marku\Desktop\StudiumVault\Semester6\Bachelorarbeit\Code\Extracting-Phylogenies-from-Images-using-AI\data_collection\dataset\phylo_rand_distances\data_2025-06-25-00-24-55-590431\phylo_tree_2025-06-25-00-24-55-590431.jpg"
-    # ete3_rand_dist = r"C:\Users\marku\Desktop\StudiumVault\Semester6\Bachelorarbeit\Code\Extracting-Phylogenies-from-Images-using-AI\data_collection\dataset\ete3_rand_distances\data_2025-06-25-00-22-24-082030\ete3_tree_2025-06-25-00-22-24-082030.jpg"
-    # # example images for transformation testing
-    # test transformations
-    # test_transforms(transform_2, ete3_20_taxa)
+    if __name__ == "__main__":
+        # Finetuning
+        # load a fine-tuned image captioning model, corresponding tokenizer, image processor and trainer
+        
+        model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+        pretrained_image_processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+        tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning") # TODO: gpt2 from huggingface instead?
+        special_tokens = ["(", ")", ";", ",", ":"] # TODO: add special tokens or not?
+        tokenizer.add_tokens(special_tokens, special_tokens=True)
+        model.decoder.resize_token_embeddings(len(tokenizer))
+        dataset_path = r".\data_collection\dataset_test\ete3_20_taxa"
+        dataset = preprocess_dataset(
+            dataset=load_dataset(dataset_path=dataset_path), 
+            tokenizer=tokenizer, 
+            image_processor=pretrained_image_processor
+        )
+        # use set_transform to apply image augmentations on the fly during training instead of map() to save disk space
+        # train in 2 stages: baseline and then robustness => augment_1_ar then augment_2 (with dropout etc.)
+        # TODO: album expects a np array not a tensor, turn tensor back into np.array
+        dataset.set_transform(transform=transform_1_ar)
+        # split dataset into 80% training and 20% evaluation
+        split_dataset = dataset.train_test_split(test_size=0.2)
+        train_dataset = split_dataset["train"]
+        eval_dataset = split_dataset["test"]
+        # set the training arguments 
+        # taken from basic training example by huggingface: https://deepwiki.com/huggingface/transformers/3.1-trainer-class
+        training_args = TrainingArguments(
+            output_dir="./results",
+            learning_rate=5e-5,
+            per_device_train_batch_size=16,
+            per_device_eval_batch_size=64,
+            num_train_epochs=3,
+            weight_decay=0.01,
+            # TODO: load_best_model_at_end = True?
+            # TODO: evaluation_strategy = "steps"?
+        )
+
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            # TODO: data collator?
+            eval_dataset=eval_dataset,
+            tokenizer=tokenizer,
+            compute_metrics=compute_metrics,
+        )
+        print(f"[{time}] main(): Trainer was instantiated.")
+
+        # Train the model
+        train_results = trainer.train()
+        trainer.save_model()
+        trainer.log_metrics("train", train_results.metrics)
+        trainer.save_metrics("train", train_results.metrics)
+        trainer.save_state()
+        
+        ########## Testing ##########
+        ete3_20_taxa = r"C:\Users\marku\Desktop\StudiumVault\Semester6\Bachelorarbeit\Code\Extracting-Phylogenies-from-Images-using-AI\data_collection\dataset_test\ete3_20_taxa\data_2025-06-25-00-25-53-945410\ete3_tree_2025-06-25-00-25-53-945410.jpg"
+        phylo_20_taxa = r"C:\Users\marku\Desktop\StudiumVault\Semester6\Bachelorarbeit\Code\Extracting-Phylogenies-from-Images-using-AI\data_collection\dataset_test\phylo_20_taxa\data_2025-06-25-00-25-27-260802\phylo_tree_2025-06-25-00-25-27-260802.jpg"
+        # # example images for transformation testing
+        # long_newick = "(((((Sinorhizobium_fredii:1,Clostridium_baratii:1):1,((((Schaalia_odontolytica:1,Halanaerobium:1):1,Pylaiella:1):1,Trianthema:1):1,Arenaria:1):1):1,Bertholletia:1):1,Pastinaca:1,(((Trochodendron_aralioides:1,Alternaria_alternata:1):1,(Malacosoma_americanum:1,Xenopus_borealis:1):1):1,Coluber_constrictor:1):1):1,(((Afropavo_congensis:1,Paridae:1):1,Alouatta_seniculus:1):1,((Afipia_carboxidovorans:1,Desulfomicrobium_norvegicum:1):1,Centruroides_tecomanus:1):1):1);"
+        # print(get_number_tokens(long_newick)) # 236 tokens without special tokens, 261 tokens with special tokens added 
+        # test_augmentation(augment=augment_1_ar, image_path=phylo_20_taxa)
+        
 # execute main
 main()
 
